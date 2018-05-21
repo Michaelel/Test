a0c7a7fe03f2e91eedc649202047b6e9171efbaf
Michael <michaelel1411@gmail.com>
2018-05-21 19:35:00 +0300

    Add categories

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ var app = angular.module('app', [])
 app.controller('dataController', function ($scope, $http) {
   $http.get('https://demo3569733.mockable.io/').success(function (data) {
     $scope.products = data.products
+    $scope.categories = []
+    let j = 0;
+    for(var i = 0; i < data.products.length; i++) {
+      if (!($scope.categories.join().search(data.products[i].bsr_category) != -1)) {             $scope.categories[j] = data.products[i].bsr_category
+        j++
+      }
+}
   }).error(function (err) {
     throw err   
   }) 
-}) 
+})
+
+
