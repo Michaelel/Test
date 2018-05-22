4851e9a92678505a7cd2d6969251570f11dcb579
Michael <michaelel1411@gmail.com>
2018-05-22 13:35:11 +0300

    code styles refactoring

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var app = angular.module('app', [])
 
 app.controller('dataController', function ($scope, $http) {
-  $http.get('https://demo3569733.mockable.io/').success(data => {
-    $scope.products = data.products
-    $scope.categories = []
-    data.products.map(el => {
-      if (!($scope.categories.join().search(el.bsr_category) != -1)) {
-        $scope.categories.push(el.bsr_category)
-      }
+    $http.get('https://demo3569733.mockable.io/').success(data => {
+        $scope.products = data.products
+        $scope.categories = []
+        data.products.map(el => {
+            if ($scope.categories.join().search(el.bsr_category) === -1) {
+                $scope.categories.push(el.bsr_category)
+            }
+        })
+    }).error(err => {
+        throw err
     })
-  }).error(err => {
-    throw err   
-  })
-  $scope.setCategoryFilter = category => {
-    $scope.categoryFilter = { bsr_category: category }
-  }
+    $scope.setCategoryFilter = category => {
+        $scope.categoryFilter = {bsr_category: category}
+    }
 })
 
 
