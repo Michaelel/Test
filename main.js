61f9b434c16bf1720b339f6c659178317937ea19
Michael <michaelel1411@gmail.com>
2018-05-23 01:12:40 +0300

    Categories routing nearly ready

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-var app = angular.module('app', [/*'ngRoute'*/])
+var app = angular.module('app', ['ngRoute'])
 
 
-/*app.config(function ($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            template: '<h1>This is my home page</h1>',
+            controller: 'dataController'
         })
-        .when('/asd', {
-            template: '<h1>Second page</h1>',
+        .when('/:bsr_category', {
+            controller: 'dataController'
         })
-});*/
+}])
 
-app.controller('dataController', function ($scope, $http) {
-    $http.get('https://demo3569733.mockable.io/').success(data => {
-        $scope.products = data.products
-        $scope.categories = []
-        data.products.map(el => {
-            if ($scope.categories.join().search(el.bsr_category) === -1) {
-                $scope.categories.push(el.bsr_category)
-            }
+app.controller('dataController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('https://demo3569733.mockable.io/')
+        .then(data => data.data)
+        .then(data => {
+            $scope.products = data.products
+            $scope.categories = []
+            data.products.map(el => {
+                if ($scope.categories.join().search(el.bsr_category) === -1) {
+                    $scope.categories.push(el.bsr_category)
+                }
+            })
         })
-    }).error(err => {
-        throw err
-    })
+        .catch(err => {
+            throw err
+        })
+
     $scope.setCategoryFilter = category => {
         $scope.categoryFilter = {bsr_category: category}
     }
-})
+}])
 
 
 
