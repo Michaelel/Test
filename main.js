48c8a00bb3cde4485f2a63660e8f24f5add1be40
Michael <michaelel1411@gmail.com>
2018-05-24 11:10:56 +0300

    Routing is ready

--- main.js
+++ main.js
@@ -6,17 +6,12 @@ app.config(['$routeProvider', function ($routeProvider) {
         .when('/', {
             controller: 'dataController'
         })
-        .when('/:search.name/:bsr_category', {
-            controller: 'dataController'
-        })
-
-
-}])
+    }])
 
 app.controller('dataController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     $http.get('https://demo3569733.mockable.io/')
-        .then(data => data.data)
-        .then(data => {
+        .then((data) => (data.data))
+        .then((data) => {
             $scope.products = data.products
             $scope.categories = []
             data.products.map(el => {
@@ -24,33 +19,63 @@ app.controller('dataController', ['$scope', '$http', '$location', function ($sco
                     $scope.categories.push(el.bsr_category)
                 }
             })
+            $scope.locationSearch = ''
+            $scope.locationCategory = ''
+            $scope.addr = $location.path().split('/')
+            if ($scope.addr[2]) {
+                $scope.locationSearch = $scope.addr[2]
+                $scope.locationCategory = $scope.addr[1]
+            } else if ($scope.categories.indexOf($scope.addr[1]) != -1) {
+                $scope.locationCategory = $scope.addr[1]
+                $scope.locationSearch = ''
+            } else if ($scope.addr[2]) {
+                $scope.locationSearch = $scope.addr[1]
+                $scope.locationCategory = ''
+            } else {
+                $scope.locationSearch = ''
+                $scope.locationCategory = ''
+            }
         })
         .catch(err => {
             throw err
         })
     $scope.categoryLocation = ''
     $scope.setCategoryFilter = category => {
-        $scope.categoryFilter = {bsr_category: category}
-        //$scope.loc = $location.path().slice(1,$location.path().length)
         $scope.categoryLocation = category + '/'
-
+        $scope.locationCategory = category
+        $scope.addr = $location.path().split('/')
+        console.log($scope.addr);
+        if ($scope.addr[2]) {
+            $scope.locationSearch = $scope.addr[2]
+        } else if ($scope.categories.indexOf($scope.addr[1]) != -1) {
+            $scope.locationSearch = ''
+        } else if ($scope.addr[1]) {
+            $scope.locationSearch = $scope.addr[1]
+        } else {
+            $scope.locationSearch = ''
+        }
+        if(category === ''){
+            $scope.searchLocation = ''
+            $scope.locationSearch = ''
+        } else {
+            $scope.searchLocation = '/' + $scope.locationSearch
+        }
     }
 
+    $scope.searchLocation = ''
     $scope.setSearchFilter = inputName => {
-        $scope.searchFilter = {name: inputName}
+        $scope.searchLocation = '/' + inputName
+        $scope.locationSearch = inputName
+        $scope.addr = $location.path().split('/')
+        console.log($scope.addr);
+        if ($scope.addr[2]) {
+            $scope.locationCategory = $scope.addr[1]
+        } else if ($scope.categories.indexOf($scope.addr[1]) != -1) {
+            $scope.locationCategory = $scope.addr[1]
+        } else {
+            $scope.locationCategory = ''
+        }
     }
-    // if (!$scope.categoryFilter) {
-    //     $location.path($scope.searchFilter)
-    // } else {
-    //     $location.path($scope.categoryFilter + '/' + $scope.searchFilter)
-    //
-    // }
-
-    // $scope.searchRoute = name => {
-    //     $scope.locName = $location.path(name)
-    //     //console.log($scope.locSearch)
-    // }
-
 }])
 
 
